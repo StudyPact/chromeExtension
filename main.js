eea1ee5ce53ba6ee370c77f214084f4913078648
Tobias Hoenisch <tobias@hoenisch.at>
2014-11-21 11:51:12 +0900

    basic url tracking

---
+++ main.js
@@ -0,0 +1,20 @@
+debugger;
+
+console.log("LOADED");
+var activeTabID = 0;
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.active)
+    console.log("TAB UPDATED:", tab.url);
+});
+
+chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.active)
+    console.log("TAB CREATED:", tab.url);
+});
+
+chrome.tabs.onActivated.addListener(function(tabId) {
+  activeTabID = tabId;
+  chrome.tabs.getSelected(null, function(tab) {
+    console.log("TAB ACTIVATED:", tab.url);
+  });
+});

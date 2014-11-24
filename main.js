956dea7c0078ff713570c7d9d3c6db17511896a5
Tobias Hoenisch <tobias@hoenisch.at>
2014-11-25 06:36:34 +0900

    error prevention

--- main.js
+++ main.js
@@ -3,18 +3,19 @@ debugger;
 console.log("LOADED");
 var activeTabID = 0;
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.active)
+  if (tab && tab.active)
     console.log("TAB UPDATED:", tab.url);
 });
 
 chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.active)
+  if (tab && tab.active)
     console.log("TAB CREATED:", tab.url);
 });
 
 chrome.tabs.onActivated.addListener(function(tabId) {
   activeTabID = tabId;
   chrome.tabs.getSelected(null, function(tab) {
-    console.log("TAB ACTIVATED:", tab.url);
+    if (tab)
+      console.log("TAB ACTIVATED:", tab.url);
   });
 });

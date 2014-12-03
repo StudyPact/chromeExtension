ba06b07ff95203de8cded54710ebf521a3c07ac8
Tobias Hoenisch <tobias@hoenisch.at>
2014-12-03 11:31:20 +0900

    status update via chromeBus

--- code/js/modules/authController.js
+++ code/js/modules/authController.js
@@ -4,6 +4,7 @@ var $ = require('../libs/jquery');
 var AuthController = {};
 var config = require("./config");
 var bus = require("./lib/simpleBus");
+var chromeBus = require("./lib/chromeBus");
 
 AuthController.saveToken = function(accessToken) {
   var deferred = Q.defer();
@@ -39,7 +40,7 @@ AuthController.login = function(email, password) {
     return AuthController.saveToken(token)
     .then(function(){
       console.log("Emitting status:update hasCookie");
-      bus.emit("state:update", "hasCookie");
+      chromeBus.emit("state:update", "hasCookie");
     });
   });
 };

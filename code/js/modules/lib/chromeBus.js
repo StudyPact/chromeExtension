d2312dac90d33c5bc22aacfc045eac40f3f86999
Tobias Hoenisch <tobias@hoenisch.at>
2014-12-04 16:31:59 +0900

    more power to the chromebus

--- code/js/modules/lib/chromeBus.js
+++ code/js/modules/lib/chromeBus.js
@@ -1,9 +1,31 @@
 var ChromeBus = {};
 var messageHandlerMapping = {};
 
+/*
+  This class behaves similar to an eventEmitter
+  wrapped around the chrome messaging bus. You can even
+  use callbacks as the last parameter
+
+  ex1:
+  bus.on("message", function(data){
+    console.log(data); 
+  });
+  bus.emit("message", {"awesome":"data"})
+
+  ex2:
+  bus.on("giveMeMore", function(callback){
+    callback({"awesome":"data"});
+  })
+  bus.emit("giveMeMore", function(data){
+    console.log(data);
+  })
+ */
 ChromeBus.on = function(message, handler) {
-  var wrapper = function(request) {
+  var wrapper = function(request,sender,sendResponse) {
     if (request.message === message) {
+      if (sendResponse){
+        request.data.push(sendResponse);
+      }
       handler.apply(this,request.data);
     }
   };
@@ -20,11 +42,20 @@ ChromeBus.removeListener = function(message, handler) {
 
 ChromeBus.emit = function(message) {
   var args = Array.prototype.slice.call(arguments, 1);
+  var last;
+  if (args[args.length-1] instanceof Function){
+    last = args.pop();
+  }
   var content = {
     data: args,
     message: message
   };
-  chrome.runtime.sendMessage(content);
+  if (last){
+    chrome.runtime.sendMessage(content, last);
+  }
+  else{
+    chrome.runtime.sendMessage(content);
+  }
 };
 
 

9132f28ffd84bd8eb9fad6b1ffff551312519096
Toby Hoenisch <tobias@hoenisch.at>
2014-12-21 13:44:46 +0100

    remove debug & fix description & update version

--- code/js/content.js
+++ code/js/content.js
@@ -1,8 +1,4 @@
 ;(function() {
-  console.log('CONTENT SCRIPT WORKS!');
-
-  var $ = require('./libs/jquery');
+  require('./libs/jquery');
   //var msg = require('./modules/bus/msg');
-
-  console.log('jQuery version:', $().jquery);
 })();
